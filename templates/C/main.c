21428d81e4c70fba543887a1e2a230c5ec3db289
somang0813-ui <somang0813@gmail.com>
2025-12-26 18:38:21 +0900

    Update main.c

--- templates/C/main.c
+++ templates/C/main.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(void)
+{
+    int n;
+    if (scanf("%d", &n) != 1)
+        return 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        scanf("%d", &x);
+        // 처리
+    }
+
+    printf("done\n");
+    return 0;
+}
